dcf1fc18c8b0eb7c9388f33b95890d4f0eb34839
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-11-30 09:43:43 +0100

    Clear tmp folder

--- server/main.ts
+++ server/main.ts
@@ -11,7 +11,8 @@ app.use('/', express.static(path.resolve('../app/dist')))
 app.post('/analyse', upload.single('file'), async function (req, res) {
     fs.writeFile("tmp/" + req.file.originalname, req.file.buffer, function (err) {
         if (err) {
-            console.log("SAVE: "  + err);
+            console.log("SAVE: " + err);
+            clearTmp()
             return res.redirect(503, '/')
         }
         console.log("File saved");
@@ -21,12 +22,29 @@ app.post('/analyse', upload.single('file'), async function (req, res) {
         keyFilename: 'key.json'
     });
 
-    const result = await client.labelDetection("tmp/" + req.file.originalname).catch(err => console.error("READ: " + err));
+    const result = await client.labelDetection("tmp/" + req.file.originalname).catch(err => {
+        console.error("READ: " + err)
+        clearTmp()
+        return res.redirect(503, '/')
+    });
     console.log(result)
     // const labels = result[0].labelAnnotations;
     // console.log('Labels:');
     // labels.forEach(label => console.log(label));
+    clearTmp()
     return res.redirect(200, '/')
 });
 
 app.listen(4200, () => console.log('Server running http://localhost:4200'));
+
+function clearTmp() {
+    fs.readdir("tmp", (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join("tmp", file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+}
